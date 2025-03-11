583a0cb556d9a2cac997f26974789afee1c4f14f
Tiago Guedes <tiagosg2599@gmail.com>
2025-03-10 21:31:03 -0300

    repository interface (3-7)

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
-import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import { appRoutes } from "./http/routes";
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: 'Tiago Guedes',
-    email: 'tiago@email.com'
-  },
-})
+app.register(appRoutes)
