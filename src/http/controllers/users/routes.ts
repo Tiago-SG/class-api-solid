04456be3b05c727ee4bbe99762d663660a436191
Tiago Guedes <tiagosg2599@gmail.com>
2025-04-14 19:02:48 -0300

    RBAC tests (7-4)

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,9 +3,11 @@ import { FastifyInstance } from 'fastify'
 import { authenticate } from './authenticate'
 import { verifyJwt } from '../../middlewares/verify-jwt'
 import { profile } from './profile'
+import { refresh } from './refresh'
  
  export async function userRoutes(app: FastifyInstance) {
    app.post('/users', register)
    app.post('/sessions', authenticate)
    app.get('/me', { onRequest: [verifyJwt] }, profile)
+   app.patch('/token/refresh', refresh)
  }
